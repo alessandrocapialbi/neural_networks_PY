9fdcfa41d5fa3daffb41a1a7cc36edc55e584fa2
Alessandro Capialbi <44204943+alessandrocapialbi@users.noreply.github.com>
2020-04-10 17:04:48 +0200

    definition of slope

--- venv/Neural network.py
+++ venv/Neural network.py
@@ -1,3 +1,15 @@
 def cost(b):
     return (b-4) ** 2
 
+def num_slope(b):
+    h = 0.0001
+    return (cost(b+h) - cost(b))/h
+
+def slope(b):
+    return 2 * (b-4)
+
+b = -20 
+for i in range(40):
+    b = b - .1 * slope(b)
+    print (b)
+
